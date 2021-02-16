c55a2adab39330bf433ef0c3ec18fb99b11c57bf
iamajaynayak <63513168+iamajaynayak@users.noreply.github.com>
2021-02-16 12:06:16 +0530

    testing form input

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import "./App.css";
 function App() {
   const [counter, setCounter] = useState(0);
 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const increaseCounter = () => {
-    setCounter((prevcounter) => prevcounter + 1);
+    setCounter((prevcounter) => {
+      console.log(prevcounter);
+      return prevcounter + 1;
+    });
   };
 
   const decreaseCounter = () => {
     setCounter((prevcounter) => {
+      console.log(prevcounter);
       if (prevcounter >= 1) {
         return prevcounter - 1;
       } else {
@@ -17,11 +24,47 @@ function App() {
       }
     });
   };
+
+  const onUsernameChanged = (event) => {
+    setUsername(() => event.target.value);
+  };
+
+  const onPasswordChanged = (event) => {
+    setPassword(() => event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log("form submitted");
+  };
+
+  console.log(username, password);
+
   return (
     <div className="App">
-      <button onClick={decreaseCounter}>-</button>
-      <h2>{counter}</h2>
-      <button onClick={increaseCounter}>+</button>
+      <div className="counter">
+        <button onClick={decreaseCounter}>-</button>
+        <h2>{counter}</h2>
+        <button onClick={increaseCounter}>+</button>
+      </div>
+
+      <div className="input-form">
+        <input
+          type="text"
+          className="username"
+          name="username"
+          placeholder="username"
+          onChange={onUsernameChanged}
+        />
+        <input
+          type="password"
+          className="password"
+          name="password"
+          placeholder="password"
+          onChange={onPasswordChanged}
+        />
+        <button onClick={onSubmit}>log In</button>
+      </div>
     </div>
   );
 }
